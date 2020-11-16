89ae7d8895925a2266a3e808357de47e48a4a7f2
Vincent Partimbene <71456931+vpartimb@users.noreply.github.com>
2020-11-16 17:03:12 +0100

    TP3

--- src/main/scala/paristech/Trainer.scala
+++ src/main/scala/paristech/Trainer.scala
@@ -1,8 +1,11 @@
 package paristech
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, MulticlassClassificationEvaluator}
+import org.apache.spark.ml.{Pipeline, PipelineModel, feature => P}
 
 object Trainer {
 
@@ -43,7 +46,118 @@ object Trainer {
       *
       ********************************************************************************/
 
-    println("hello world ! from Trainer")
+    val df = spark
+      .read
+      .parquet("/mnt/c/Users/vince/Google Drive/MS BGD/Cours/INF729_Hadoop/spark_project_kickstarter_2020_2021/data/prepared_trainingset/")
+
+    val tokenizer = new P.RegexTokenizer()
+      .setPattern("\\W+")
+      .setGaps(true)
+      .setInputCol("text")
+      .setOutputCol("tokens")
+
+    val stopWordsRemover = new P.StopWordsRemover()
+      .setInputCol("tokens")
+      .setOutputCol("filtered")
+
+    val countVectorizer = new P.CountVectorizer()
+      .setMinDF(65)
+      .setMinTF(1)
+      .setInputCol("filtered")
+      .setOutputCol("tf")
+
+    val idf = new P.IDF()
+      .setInputCol("tf")
+      .setOutputCol("tfidf")
+
+    /** CONVERTING STRINGS TO NUMERICAL VALUES **/
+
+    val countryIndexer = new P.StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+      .setHandleInvalid("keep")
+
+    val currencyIndexer = new P.StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+      .setHandleInvalid("keep")
+
+    /** VECTOR ASSEMBLER **/
+
+    val assembler = new P.VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "country_indexed", "currency_indexed"))
+      .setOutputCol("features")
+
+    /** LOGISTIC REGRESSION **/
+
+    val lr = new LogisticRegression()
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_predictions")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(100)
+
+    /** PIPELINE **/
+
+    val pipeline: Pipeline = new Pipeline()
+      .setStages(Array(
+        tokenizer,
+        stopWordsRemover,
+        countVectorizer,
+        idf,
+        countryIndexer,
+        currencyIndexer,
+        assembler,
+        lr
+      ))
+
+//    val model = pipeline.fit(df)
+//    val dfTransformed = model.transform(df)
+//
+//    dfTransformed.show()
+
+    df.persist()
+    val Array(train, test) = df.randomSplit(Array(0.9, 0.1), seed = 42)
+    val model = pipeline.fit(train)
+
+    def evaluateModel(model: PipelineModel, df: DataFrame) = {
+      val predictions = model.transform(df)
+      val eval_auc = new BinaryClassificationEvaluator()
+        .setLabelCol("final_status")
+        .setMetricName("areaUnderROC")
+        .setRawPredictionCol("probability")
+
+      val eval_wprec = new MulticlassClassificationEvaluator()
+        .setMetricName("weightedPrecision")
+        .setLabelCol("final_status")
+        .setPredictionCol("predictions")
+
+      val eval_recall = new MulticlassClassificationEvaluator()
+        .setMetricName("weightedRecall")
+        .setLabelCol("final_status")
+        .setPredictionCol("predictions")
+
+      val eval_f1 = new MulticlassClassificationEvaluator()
+        .setMetricName("f1")
+        .setLabelCol("final_status")
+        .setPredictionCol("predictions")
+
+      val auc = eval_auc.evaluate(predictions)
+      println(s"AUC: $auc")
+      val precision = eval_wprec.evaluate(predictions)
+      println(s"Precision: $precision")
+      val recall = eval_recall.evaluate(predictions)
+      println(s"Recall: $recall")
+      val f1_score = eval_f1.evaluate(predictions)
+      println(s"F1 Score: $f1_score")
+    }
+
+    evaluateModel(model, test)
 
   }
 }
